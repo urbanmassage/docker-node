13a06c1542d9a2dc513b2f7c0d4410b28507719d
Giles Williams <giles.williams@gmail.com>
2016-04-25 21:24:12 +0100

    Using templates

--- test-build.sh
+++ test-build.sh
@@ -18,40 +18,34 @@ fatal() {
 
 cd $(cd ${0%/*} && pwd -P);
 
-versions=( "$@" )
-if [ ${#versions[@]} -eq 0 ]; then
-  versions=( */ )
-fi
-versions=( "${versions[@]%/}" )
+versions=("0.10.44" "5.11.0")
+variants=("slim")
 
 for version in "${versions[@]}"; do
-  if [[ "$version" == "docs" ]]; then
-    continue
-  fi
-
-  NODE_VERSION=$(cat $version/Dockerfile | grep "ENV NODE_VERSION" | awk '{split($2,a,"=");print a[2]}')
-  MINOR_VERSION=$(echo $NODE_VERSION | awk '{split($0,b,".");print b[1]"."b[2]}')
+  MINOR_VERSION=$(echo $version | awk '{split($0,b,".");print b[1]"."b[2]}')
   MAJOR_VERSION=$(echo $MINOR_VERSION | awk '{split($0,b,".");print b[1]}')
 
-  variants=$(ls -d $version/*/ | awk -F"/" '{print $2}')
-
   for variant in $variants; do
-    info "Building $NODE_VERSION-$variant variant..."
-    docker build -q -t urbanmassage/node:$NODE_VERSION-$variant $version/$variant
-    docker tag urbanmassage/node:$NODE_VERSION-$variant urbanmassage/node:$NODE_VERSION-$MINOR_VERSION
+    mkdir -p $version/$variant
+
+    cat Dockerfile-template-$variant | sed 's/%%NODE_VERSION%%/'$version'/' > $version/$variant/Dockerfile
+
+    info "Building $version-$variant variant..."
+    docker build -q -t urbanmassage/node:$version-$variant $version/$variant
+    docker tag urbanmassage/node:$version-$variant urbanmassage/node:$version-$MINOR_VERSION
 
     if [ $MAJOR_VERSION != "0" ]; then
-      docker tag urbanmassage/node:$NODE_VERSION-$variant urbanmassage/node:$NODE_VERSION-$MAJOR_VERSION
+      docker tag urbanmassage/node:$version-$variant urbanmassage/node:$version-$MAJOR_VERSION
     fi
 
     if [[ $? -gt 0 ]]; then
-      fatal "Build of $NODE_VERSION-$variant failed!"
+      fatal "Build of $version-$variant failed!"
     else
-      info "Build of $NODE_VERSION-$variant succeeded."
+      info "Build of $version-$variant succeeded."
     fi
 
-    OUTPUT=$(docker run --rm -it urbanmassage/node:$NODE_VERSION-$variant node -e "process.stdout.write(process.versions.node)")
-    if [ "$OUTPUT" != "$NODE_VERSION" ]; then
+    OUTPUT=$(docker run --rm -it urbanmassage/node:$version-$variant node -e "process.stdout.write(process.versions.node)")
+    if [ "$OUTPUT" != "$version" ]; then
       fatal "Test of $tag-$variant failed!"
     else
       info "Test of $tag-$variant succeeded."
