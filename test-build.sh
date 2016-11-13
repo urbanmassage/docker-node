72fbb62f5f95c048bf29e9f36d8b5a4c1f14e683
Louy Alakkad <louy@alakkad.me>
2016-11-13 13:50:09 +0000

    Update test-build.sh

--- test-build.sh
+++ test-build.sh
@@ -20,7 +20,8 @@ cd $(cd ${0%/*} && pwd -P);
 
 # https://nodejs.org/dist/latest-v4.x/
 # https://nodejs.org/dist/latest-v6.x/
-versions=("4.6.0" "6.9.1" "7.0.0")
+# https://nodejs.org/dist/latest-v7.x/
+versions=("4.6.2" "6.9.1" "7.1.0")
 variants=("slim")
 
 for version in "${versions[@]}"; do
